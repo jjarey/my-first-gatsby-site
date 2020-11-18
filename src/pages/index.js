0d108dddb79c365b91c64cc5310c7cfb66b253f4
N7511 <n7511@student.jamk.fi>
2020-11-18 15:47:35 +0200

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+// The Gatsby <Link /> component is for internal linking between pages within the site.
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
+      <Link to="/about/">About</Link>
+      <Header headerText="Hello There!" />
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
 }
